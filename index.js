ded14c58c1bfca97edd52daf0128b72e518ddc6d
Lisu <everyaccountholder@wp.pl>
2022-04-15 17:43:25 +0200

    głównie kosmetyka

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const chalk = require("chalk");
 const fs = require('fs');
 const readline = require('readline').createInterface({input: process.stdin, output: process.stdout});
 const package = require("./package.json");
-const editJson = require("./resources/functions/editJson.js");
 
-console.clear();
-console.log(chalk.magenta(`milionerzy-js, wersja v${package.version}.`));
 
 if(!fs.existsSync("./user/info.json")) {
     let json = {
@@ -14,6 +11,7 @@ if(!fs.existsSync("./user/info.json")) {
         xp: 0
     }
     json = JSON.stringify(json, null, 2);
+
     fs.writeFile('./user/info.json', json, (err) => {
         if(err) throw new Error("Nie udało się utworzyć pliku z danymi użytkownika!");
     });
@@ -24,29 +22,31 @@ if(!fs.existsSync("./user/stats.json")) {
         wins: 0
     }
     json = JSON.stringify(json, null, 2);
+    
     fs.writeFile('./user/stats.json', json, (err) => {
         if(err) throw new Error("Nie udało się utworzyć pliku ze statystykami użytkownika!");
     });
 }
 
+console.clear();
 
-
+console.log(chalk.magenta(`milionerzy-js, wersja v${package.version}.`));
 console.log(chalk.red("1. ") + chalk.blue("Rozpocznij grę!"));
 console.log(chalk.red("2. ") + chalk.blue("Sprawdź mój profil!"));
-console.log(chalk.red.bold("Sklep pojawi się w grze wkrótce!"));
-// console.log(chalk.red("3. ") + chalk.blue("Zajrzyj do sklepu!"));
+console.log(chalk.red.bold("Sklep pojawi się w grze wkrótce!")); // console.log(chalk.red("3. ") + chalk.blue("Zajrzyj do sklepu!"));
 console.log(chalk.blue("Inna odpowiedź - wyjście z gry."));
+
 readline.question("Więc, co robimy?: ", answer => {
-    if(answer == "1") {
-        readline.close();
-        require("./game.js").execute();
-    } else if(answer == "2") {
-        readline.close();
-        require("./profile.js").execute();
-    /* } else if(answer == "3") {
-    //     readline.close();
-    //     require("./shop.js").execute();
-    */ } else {
-        process.exit(0);
+    readline.close();
+
+    switch(answer) {
+        case "1":
+            require("./game.js");
+            break;
+        case "2":
+            require("./profile.js");
+            break;
+        default:
+            process.exit();
     }
 })
